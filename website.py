bfb88c654c3765adc9381e9d874b43cadbf5c935
Ryan Weber <rweber12@emich.edu>
2023-12-28 12:28:45 -0500

    update base code

--- website.py
+++ website.py
@@ -1,8 +1,7 @@
-from Flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return "Hey there!"
 if __name__ == '__main__':
     app.run(debug=True)
-

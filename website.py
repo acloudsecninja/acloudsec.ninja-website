bb9129068b26cc5cba778c4ffb69f0e36d09ba5c
Ryan Weber <rweber12@emich.edu>
2024-01-03 15:09:11 -0500

    update codebase

--- website.py
+++ website.py
@@ -1,3 +1,9 @@
-import streamlit as st
+import arrr
+from pyscript import document
 
-st.write("Hello world")
+
+def translate_english(event):
+    input_text = document.querySelector("#english")
+    english = input_text.value
+    output_div = document.querySelector("#output")
+    output_div.innerText = arrr.translate(english)
